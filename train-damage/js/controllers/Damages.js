05d4b76530fe21e03d913f4a5b30cd02380c4299
Christophe Bram <cbr@appearnetworks.com>
2013-05-27 15:19:48 +0200

    Updated train-damage to the latest version of the step-by-step tutorial

--- train-damage/js/controllers/Damages.js
+++ train-damage/js/controllers/Damages.js
@@ -4,13 +4,13 @@ AIQ.Spine.Controller.sub({
     },
 
     init: function () {
-        TD.Damage.bind("refresh create", this.proxy(this.render));
-        TD.Damage.fetch();
+        TD.DamageReport.bind("refresh create", this.proxy(this.render));
+        TD.DamageReport.fetch();
     },
 
     destroy: function() {
         // Unbind all Spine and AIQ bindings here
-        TD.Damage.unbind();
+        TD.DamageReport.unbind();
 
         // Calling parent
         this.constructor.__super__.destroy.apply(this, arguments);
@@ -29,10 +29,10 @@ AIQ.Spine.Controller.sub({
             selectedTrain = TD.Train.find(this.trainId);
 
             // Set title in nav bar
-            AIQ.Core.App.setTitle("Train " + selectedTrain.trainNumber);
+            AIQ.Core.App.setTitle("Train " + selectedTrain.number);
 
             // See http://spinejs.com/api/models for the documentation on Spine Model functions, including "findAllByAttribute(name, value)"
-            var trainDamages = TD.Damage.findAllByAttribute("trainId", this.trainId);
+            var trainDamages = TD.DamageReport.findAllByAttribute("trainId", this.trainId);
 
             this.renderTemplate({
                 damageCount: trainDamages.length,
